bd1b7211e9770a1ad05f0237096c11eb0f8a094f
HarshaVardhan <athaluri.harsha@gmail.com>
2022-02-09 08:06:53 +0530

    Eureka example

---
+++ EurekaDemo/userservice/src/main/java/com/training/userservice/dto/UserDto.java
@@ -0,0 +1,37 @@
+package com.training.userservice.dto;
+
+import java.util.List;
+
+public class UserDto {
+
+	private int id;
+	private String name;
+	private String adress;
+	private List<OrdersDto> orders;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	public List<OrdersDto> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<OrdersDto> orders) {
+		this.orders = orders;
+	}
+	
+	
+}
