ad7060fd2490b4040444f83702932bda2f204c2b
HarshaVardhan <athaluri.harsha@gmail.com>
2022-02-13 11:32:15 +0530

    sleuth + zipkin

---
+++ sleuth+Zipkin/orderservice/src/main/java/com/training/orderservice/model/Orders.java
@@ -0,0 +1,46 @@
+package com.training.orderservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Orders {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int userId;
+	private String orderType;
+	private String orderStatus;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getOrderType() {
+		return orderType;
+	}
+	public void setOrderType(String orderType) {
+		this.orderType = orderType;
+	}
+	public String getOrderStatus() {
+		return orderStatus;
+	}
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
+	
+	
+	
+	
+}
